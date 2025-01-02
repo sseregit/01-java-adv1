b3b29bd9b26054da7bf6b3899f45143dec8f11ef
장연식 <sseregit@knowwheresoft.com>
2025-01-02 13:38:15 +0900

    synchronized 코드 블럭

--- src/thread/sync/BankMain.java
+++ src/thread/sync/BankMain.java
@@ -7,7 +7,8 @@ public class BankMain {
 
     public static void main(String[] args) throws InterruptedException {
 //        BankAccount account = new BankAccountV1(1000);
-        BankAccount account = new BankAccountV2(1000);
+//        BankAccount account = new BankAccountV2(1000);
+        BankAccount account = new BankAccountV3(1000);
 
         Thread t1 = new Thread(new WithdrawTask(account, 800), "t1");
         Thread t2 = new Thread(new WithdrawTask(account, 800), "t2");
@@ -16,7 +17,7 @@ public class BankMain {
 
         sleep(500);
         log("t1.state = " + t1.getState());
-        log("t1.state = " + t1.getState());
+        log("t2.state = " + t2.getState());
 
         t1.join();
         t2.join();
