645e758555b4dc987241b1b046fb632d0bdac7b3
장연식 <sseregit@knowwheresoft.com>
2025-01-03 17:08:03 +0900

    생산자 소비자 문제 - 예제2 코드

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -9,10 +9,11 @@ import static util.ThreadUtils.sleep;
 public class BoundedMain {
 
     public static void main(String[] args) {
-        BoundedQueue queue = new BoundedQueueV1(2);
+//        BoundedQueue queue = new BoundedQueueV1(2);
+        BoundedQueue queue = new BoundedQueueV2(2);
 
-        producerFirst(queue);
-//        consumerFirst(queue);
+//        producerFirst(queue);
+        consumerFirst(queue);
     }
 
     private static void producerFirst(BoundedQueue queue) {
