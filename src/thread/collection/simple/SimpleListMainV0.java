3c98aab8ce925d5a3873380d2bbd590264e23df5
장연식 <sseregit@knowwheresoft.com>
2025-01-07 15:58:01 +0900

    동시성 컬렉션이 필요한 이유1 - 시작

---
+++ src/thread/collection/simple/SimpleListMainV0.java
@@ -0,0 +1,15 @@
+package thread.collection.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SimpleListMainV0 {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("a");
+        list.add("b");
+        System.out.println(list);
+    }
+}
